462e75ca3b7f737f6aa84652b989acc2af3ed69b
fenghui <fengh0409@qq.com>
2023-02-06 12:58:54 +0800

    feat: update MergeTwoSortedLists

--- 021.MergeTwoSortedLists/MergeTwoSortedLists.go
+++ 021.MergeTwoSortedLists/MergeTwoSortedLists.go
@@ -8,28 +8,25 @@ type ListNode = structures.ListNode
 func MergeTwoSortedLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	// 解法一：
 	// 类似合并两个有序数组，同时遍历两条链表，比较数值大小
-	var list3 = &ListNode{}
-	var dummy = list3
+	var dummy = &ListNode{}
+	var list3 = dummy
 
 	for list1 != nil || list2 != nil {
-		var current *ListNode
 		if list1 != nil && list2 != nil {
 			if list1.Val > list2.Val {
-				current = list2
+				list3.Next = list2
 				list2 = list2.Next
 			} else {
-				current = list1
+				list3.Next = list1
 				list1 = list1.Next
 			}
-		} else if list1 != nil && list2 == nil {
-			current = list1
-			list1 = list1.Next
-		} else if list1 == nil && list2 != nil {
-			current = list2
-			list2 = list2.Next
+		} else if list1 != nil {
+			list3.Next = list1
+			list1 = nil
+		} else {
+			list3.Next = list2
+			list2 = nil
 		}
-
-		list3.Next = current
 		list3 = list3.Next
 	}
 
