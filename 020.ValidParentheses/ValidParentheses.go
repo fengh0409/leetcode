b6202c9b903347d3701f213bf5546e0a36946967
Gary Feng <fengh0409@gmail.com>
2023-02-08 00:21:49 +0800

    feat: update

--- 020.ValidParentheses/ValidParentheses.go
+++ 020.ValidParentheses/ValidParentheses.go
@@ -2,23 +2,19 @@ package leetcode
 
 // ValidParentheses 有效的括号
 func ValidParentheses(s string) bool {
-	// 遇到左括号入栈，遇到右括号判断栈顶元素是否为对应的左括号，是则把该栈顶元素出栈，遍历结束后，栈为空，则视为合法的括号。
-	if len(s)%2 == 1 {
-		return false
-	}
-
-	var stack = make([]byte, 0)
-	// 左右括号分别以key、value存储在map中
 	var m = map[byte]byte{'(': ')', '{': '}', '[': ']'}
+	// 遇到左括号入栈，遇到右括号时判断栈顶元素是否为对应的左括号，是则把该栈顶元素出栈，遍历结束后，栈为空，则视为合法的括号。
+	var stack = make([]byte, 0)
 	for i := 0; i < len(s); i++ {
 		if _, ok := m[s[i]]; ok {
-			// 左括号入栈
+			// 若为左括号入栈
 			stack = append(stack, s[i])
 		} else {
-			// 右括号出栈
+			// 若为右括号，则移出栈顶元素
 			if len(stack) > 0 && s[i] == m[stack[len(stack)-1]] {
 				stack = stack[:len(stack)-1]
 			} else {
+				// 如果上面都if没通过，则肯定是非法都括号，直接return false
 				return false
 			}
 		}
