c13041ed9a4361d7c1edb31dbb4dd5b1f188bc4e
fenghui <fengh0409@qq.com>
2023-02-08 20:36:43 +0800

    feat: update

--- 144.BinaryTreePreorderTraversal/BinaryTreePreorderTraversal.go
+++ 144.BinaryTreePreorderTraversal/BinaryTreePreorderTraversal.go
@@ -12,6 +12,28 @@ func PreorderTraversal(root *TreeNode) []int {
 	var result = []int{}
 	traversal(root, &result)
 	return result
+
+	// 迭代法，显示声明一个栈，栈是先入后出，前序遍历是先左节点再右节点，则是右节点先入栈，左节点后入栈
+	// if root == nil {
+	// 	return []int{}
+	// }
+	// var res []int
+	// var stack = []*TreeNode{}
+	// stack = append(stack, root)
+	// for len(stack) != 0 {
+	// 	node := stack[len(stack)-1]
+	// 	stack = stack[:len(stack)-1]
+	// 	if node != nil {
+	// 		res = append(res, node.Val)
+	// 	}
+	// 	if node.Right != nil {
+	// 		stack = append(stack, node.Right)
+	// 	}
+	// 	if node.Left != nil {
+	// 		stack = append(stack, node.Left)
+	// 	}
+	// }
+	// return res
 }
 
 func traversal(root *TreeNode, result *[]int) {
