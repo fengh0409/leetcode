a1ebf563e3b5cdb4e3f955954f95165eb6c9e544
fenghui <fengh0409@qq.com>
2023-03-05 20:39:36 +0800

    feat: update sort

--- Sort/006.QuickSort.go
+++ Sort/006.QuickSort.go
@@ -3,27 +3,61 @@ package leetcode
 // QuickSort 快速排序，时间复杂度O(nlogn)，最坏情况O(n^2)，不稳定
 func QuickSort(nums []int) []int {
 	return quickSort(nums, 0, len(nums)-1)
+	//return quickSort2(nums, 0, len(nums)-1)
 }
 
-func quickSort(nums []int, left, right int) []int {
-	if left < right {
+// 基本快排
+func quickSort(nums []int, i, j int) []int {
+	if i < j {
 		// 取第一个值作为基准值
-		pivot := left
+		pivot := i
 		// 基准值后面的第一个数
-		index := pivot + 1
-		// 遍历基准值后面所有的数据，和基准值进行比较
-		for i := index; i <= right; i++ {
-			if nums[i] < nums[pivot] {
-				nums[index], nums[i] = nums[i], nums[index]
-				index++
+		left, right := pivot+1, j
+		// 遍历基准值后面所有的数据，和基准值进行比较，比基准值小的往左边放
+		for k := left; k <= right; k++ {
+			if nums[k] < nums[pivot] {
+				nums[left], nums[k] = nums[k], nums[left]
+				left++
 			}
 		}
-		// 比较一趟下来，index前半部分都比基准值小，index后半部分都比基准值大
-		nums[pivot], nums[index-1] = nums[index-1], nums[pivot]
-		// 重新设置pivot，进行分治
-		mid := index - 1
-		quickSort(nums, left, mid-1)
-		quickSort(nums, mid+1, right)
+		// 比较一趟下来，left前半部分都比基准值小，left后半部分都比基准值大
+		// 交换基准值与分治位置
+		nums[pivot], nums[left-1] = nums[left-1], nums[pivot]
+		// 重新设置边界，进行分治
+		mid := left - 1
+
+		quickSort(nums, i, mid-1)
+		quickSort(nums, mid+1, j)
+	}
+
+	return nums
+}
+
+// 双指针
+func quickSort2(nums []int, i, j int) []int {
+	if i < j {
+		pivot := i
+		left, right := i+1, j
+		for {
+			for left <= right && nums[left] < nums[pivot] {
+				left++
+			}
+			for left <= right && nums[right] > nums[pivot] {
+				right--
+			}
+
+			if left >= right {
+				break
+			}
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+			right--
+		}
+
+		nums[pivot], nums[left-1] = nums[left-1], nums[pivot]
+		mid := left - 1
+		quickSort2(nums, i, mid-1)
+		quickSort2(nums, mid+1, j)
 	}
 
 	return nums
